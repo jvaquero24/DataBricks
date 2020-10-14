8821dd02869c8d88d49d4434ba8dc6c8dd1f505b
jvaquero24 <67736596+jvaquero24@users.noreply.github.com>
2020-10-14 12:03:13 +0200

    Create Vars.tf

---
+++ Vars.tf
@@ -0,0 +1,12 @@
+variable "name_databrick" {
+  type        = string
+}
+variable "location" {
+  type        = string
+}
+variable "sku" {
+  type        = string
+}
+variable "resource_group_name" {
+  type        = string
+}
