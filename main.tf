3b03e33899b8e772eaecd6342594bae047c92026
jvaquero24 <67736596+jvaquero24@users.noreply.github.com>
2020-10-14 12:02:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+data "azurerm_resource_group" "RG" {
+  name = var.resource_group_name
+}
+
 resource "azurerm_databricks_workspace" "ADB" {
   name                = var.name_databrick
   resource_group_name = data.azurerm_resource_group.RG.name
